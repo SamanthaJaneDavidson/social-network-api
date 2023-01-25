46458e7109bd1fad7d2c00528b05410d5640d5a8
unknown <samanthajanedavidson@gmail.com>
2023-01-25 18:46:52 -0500

    Updated server file. Insomnia works now!

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const routes = require('./routes');
-const mongodb = require('mongodb').MongoClient;
 
 const PORT = 3001;
 const app = express();
 
-const connectionURI = 'mongodb://localhost/social_DB';
+const connectionURI = 'mongodb://localhost:27017/social_DB';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-mongodb.connect(
-    connectionURI,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    (err, client) => {
-      app.listen(PORT, () => {
-        console.log(`App listening at http://localhost:${PORT}`);
-      });
-    }
-  );
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social_DB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+  });
+});
